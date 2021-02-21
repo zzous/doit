0545f15f4de31bbbd4a541c9ef9ac81750e0fd9d
yusung <louis.jo@brabbit.com>
2021-02-22 00:14:31 +0900

    public path

--- vue.config.js
+++ vue.config.js
@@ -9,20 +9,10 @@ module.exports = {
     ? '/doit/'
     : '/',
 //    productionSourceMap: false,
-// //    transpileDependencies: [/node_modules[/\\\\]bootstrap[/\\\\]/],
-//     chainWebpack: config => {
-//     config.output.chunkFilename('[name].[chunkhash].js')
-//     config.plugins.delete('prefetch')
-//   },
+  //  transpileDependencies: [/node_modules[/\\\\]bootstrap-vue[/\\\\]/],
+    chainWebpack: config => {
+    config.output.chunkFilename('[name].[chunkhash].js')
+    config.plugins.delete('prefetch')
+  },
   productionSourceMap: false,
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          include: path.resolve('node_modules', 'bootstrap-vue'),
-          sideEffects: false
-        }
-      ]
-    }
-  }
 }
